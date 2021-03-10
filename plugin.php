506a34666b531f1aa7c1447544cc4501d2e95d9f
pattyok <patty.ohara@gmail.com>
2021-03-09 16:41:50 -0800

    match version

--- plugin.php
+++ plugin.php
@@ -8,7 +8,7 @@
 * Primary Branch: main
 * Description: Series of blocks designed to work with this site's custom Theme
 * Author: Patty O'Hara
-* Version: 1.1.0
+* Version: 1.1.1
 * Author URI https://carkeekstudios.com/
 * Text Domain: carkeek-blocks
 */
