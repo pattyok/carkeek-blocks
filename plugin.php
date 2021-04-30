c2501e0e6046017c2b9fcb3d658fdd75d176740d
pattyok <patty.ohara@gmail.com>
2021-04-30 11:44:33 -0700

    version bump

--- plugin.php
+++ plugin.php
@@ -8,7 +8,7 @@
 * Primary Branch: main
 * Description: Series of blocks designed to work with this site's custom Theme
 * Author: Patty O'Hara
-* Version: 1.2.32
+* Version: 1.2.33
 * Author URI https://carkeekstudios.com/
 * Text Domain: carkeek-blocks
 */
