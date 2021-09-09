48bd8cf3ef8970a12ffe29a473e567ad422d8954
pattyok <patty.ohara@gmail.com>
2021-09-09 12:50:44 -0700

    fix modal image

--- plugin.php
+++ plugin.php
@@ -8,7 +8,7 @@
  * Primary Branch: main
  * Description: Series of blocks designed to work with this site's custom Theme
  * Author: Patty O'Hara
- * Version: 1.4.7
+ * Version: 1.4.8
  * Author URI https://carkeekstudios.com/
  * Text Domain: carkeek-blocks
  */
