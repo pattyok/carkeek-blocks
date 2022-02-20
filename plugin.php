22cc997b2be42cd6557693076b98c65b446190a9
pattyok <patty.ohara@gmail.com>
2022-02-20 09:32:07 -0800

    plugin bumb

--- plugin.php
+++ plugin.php
@@ -8,7 +8,7 @@
  * Primary Branch: main
  * Description: Series of blocks designed to work with this site's custom Theme
  * Author: Patty O'Hara
- * Version: 2.2.26
+ * Version: 2.2.27
  * Author URI https://carkeekstudios.com/
  * Text Domain: carkeek-blocks
  */
