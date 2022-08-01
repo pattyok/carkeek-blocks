a1c970c40a46fe0f3ffdbc02ce949a3db790df94
pattyok <patty.ohara@gmail.com>
2022-08-01 16:05:07 -0700

    fix gallery block

--- plugin.php
+++ plugin.php
@@ -8,7 +8,7 @@
  * Primary Branch: main
  * Description: Series of blocks designed to work with this site's custom Theme
  * Author: Patty O'Hara
- * Version: 2.2.58
+ * Version: 2.2.59
  * Author URI https://carkeekstudios.com/
  * Text Domain: carkeek-blocks
  */
