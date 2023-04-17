997adf0eb31187d9e9ba1fb3d45ddd3ce5c901d8
pattyok <patty.ohara@gmail.com>
2023-04-17 14:09:35 -0700

    fix to gallery block styles

--- build/custom-archive/index.asset.php
+++ build/custom-archive/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '89575a00d71d0a3a66cf');
+<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => 'de54c291b9a53e21e546');
