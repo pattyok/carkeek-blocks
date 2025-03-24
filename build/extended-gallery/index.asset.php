2ba365dbe36bf01d8978644d2e60e8f1970c6619
POK <pok@Pattys-Mac-Studio.local>
2025-03-24 09:43:18 -0700

    version bump

--- build/extended-gallery/index.asset.php
+++ build/extended-gallery/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '3c715529a63dc90cc88e');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '05935a9ec592d25ac4a7');
--- carkeek-blocks.php
+++ carkeek-blocks.php
@@ -8,7 +8,7 @@
  * Primary Branch: main
  * Description: Series of blocks designed to work with this site's custom Theme
  * Author: Patty O'Hara
- * Version: 3.2.48
+ * Version: 3.2.49
  * Author URI https://carkeekstudios.com/
  * Text Domain: carkeek-blocks
  */
