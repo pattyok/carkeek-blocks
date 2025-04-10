7de176ec0c4a1c7df1ab1fdec9607cefbd51573a
POK <pok@Pattys-Mac-Studio.local>
2025-04-10 10:17:17 -0700

    fix to gallery spans

--- build/extended-gallery/index.asset.php
+++ build/extended-gallery/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '05935a9ec592d25ac4a7');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'd9b3dbb071e11fec6bb1');
