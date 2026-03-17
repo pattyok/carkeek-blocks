c073ee236f57ee3800d6f853f541a5a0465e355d
POK <pok@Pattys-Mac-Studio.local>
2026-03-17 13:11:40 -0700

    add swap to image compare

--- build/image-compare/index.asset.php
+++ build/image-compare/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => '9204d1bdfd05d619f8c9');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => 'f5708d22e48721fecb4d');
