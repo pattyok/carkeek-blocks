ff9726037614ac7275d8e81c371928a80a2b4164
POK <pok@Pattys-Mac-Studio.local>
2026-03-18 09:31:02 -0700

    update version on the fixed image block

--- build/fixed-image/index.asset.php
+++ build/fixed-image/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '8656c7e25d8cff57f802');
+<?php return array('dependencies' => array('lodash', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '7f35b198fb07d321fa5e');
