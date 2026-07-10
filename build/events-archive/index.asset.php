5b10f8a067fbd4b8d833f57f67bc962ef9f11366
POK <pok@Pattys-Mac-Studio.local>
2026-07-09 20:56:20 -0700

    some changes to event archive to support facetwp

--- build/events-archive/index.asset.php
+++ build/events-archive/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n', 'wp-server-side-render'), 'version' => '06e816c2f03e7506031c');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n', 'wp-server-side-render'), 'version' => '85088c06275d48b2d72d');
