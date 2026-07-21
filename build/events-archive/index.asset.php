cfea05b055a3937bd1bca93119065cb1e2afb07e
POK <pok@Pattys-Mac-Studio.local>
2026-07-21 15:35:02 -0700

    bumping version

--- build/events-archive/index.asset.php
+++ build/events-archive/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n', 'wp-server-side-render'), 'version' => '85088c06275d48b2d72d');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n', 'wp-server-side-render'), 'version' => '274cb14424e65a786416');
